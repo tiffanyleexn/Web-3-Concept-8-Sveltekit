566b670a370027358602a808ef19e81d1b821c95
tiffanyleexn <leexuenni@gmail.com>
2025-11-02 11:30:45 +0800

    fork

---
+++ src/Brand.svelte
@@ -0,0 +1,8 @@
+<p> The Brands of the items </p>
+<ul>
+    <li>Dell</li>
+    <li>Contigo</li>
+    <li>Apple</li>
+    <li>Moleskine</li>
+    <li>Pilot</li>
+</ul>
---
+++ src/Item.svelte
@@ -0,0 +1,8 @@
+<p> The Items around Me </p>
+<ol>
+    <li>Laptop</li>
+    <li>Water Bottle</li>
+    <li>Smartphone</li>
+    <li>Notebook</li>
+    <li>Pen</li>
+</ol>
